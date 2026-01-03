6cdf7c75149b1902843056810bfcb38b54fc3dc7
Klaywin Ryan A. Dias <klaywindias@gmail.com>
2026-01-03 17:18:26 -0300

    Implement Employee class with various functionalities
    Added Employee class with methods for entry/exit registration, stock review, overtime calculation, vacation requests, and salary raise requests.

--- modelos.py
+++ modelos.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+#  DATA DA ATUALIZAÇÃO: 02.01.2026 as 22:30 da noite
 class Client: 
     def __init__(self, name, cnpj, id_client, credit_limit, costumer_preferences, status_client, registration_date, address, phone, client_type, loyalty_points=0):
         self.__name = name 
@@ -189,9 +190,130 @@ class Client:
         days = (datetime.now() - last_purchase_date).days
         return "Cliente Ativo" if days <= 90 else "Cliente Inativo"
 
+#  DATA DA ATUALIZAÇÃO: 03.01.2026 as 17:17 da tarde 
+class Employee:
+    def __init__(self, name, shift, cpf, salary, id_employee, departament, status_employee, admission_date, contract_type, position, meta_monthly, overtime, hours_worked):
+        self.__name = name 
+        self.__shift = shift
+        self.__id_employee = id_employee
+        self.__departament = departament
+        self.__salary = salary 
+        self.__cpf = cpf
+        self.__status_employee = status_employee
+        self.__admission_date = admission_date
+        self.__contract_type = contract_type
+        self.__position = position
+        self.__meta_monthly = meta_monthly
+        self.__overtime = overtime
+        self.__hours_worked = hours_worked
+        self.__entry_time = None
+
+
+    def rehister_entry(self):
+        if self.__entry_time is not None:
+            return "Sua entrada já foi registrada..."
+
+        self.__entry_time = datetime.now()
+        return "A entrada foi regiistrada com sucesso!."
     
-#_________________________________________________________________________________________________________________________________________
+
+    def register_exit(self):
+        if not self.__entry_time:
+            return "A entrada não foi registrada"
+        
+        exit_time = datetime.now()
+        worked_hours = (exit_time - self.__entry_time).seconds / 3600
+        
+        self.__hours_worked += worked_hours
+        
+        if worked_hours > 8:
+            self.__overtime += worked_hours - 8
+            
+        self.__entry_time = None
+        return "A Saída foi registrada"
+        
+         
+    def review_stock(self, stock):
+
+        reviewed_stock  = []
+        
+        for item in stock:
+            reviewed_stock.append({
+                
+                "product": item["product"],
+                "pallet_quantity": item["pallet_quantity"],
+                "storage_type": item["storage_type"],
+                "condition": item["condition"]
+            })
+                    
+        return reviewed_stock
+               
+#calculo da hora extra com base no tipo de dia trabalhado - normal, domingo o feriado:
+#é considerado aspenas as horas que passam da jornada de 8 horas e aplica o adicional de 50 ou 100
+
+    def calculate_overtime(self, day_type = "Normal"):
+        standard_daily_hours = 8
+        
+        if self.__hours_worked <= standard_daily_hours:
+            return 0.0
     
+        extra_hours = self.__hours_worked - standard_daily_hours
+        hourly_value = self.__salary / 200
+        
+        if day_type == "feriado" or day_type == "domingo":
+            overtime_rate = 2.0 # 100 por cento 
+            
+        else: 
+            overtime_rate = 1.5
+        
+        overtime_value = extra_hours * hourly_value * overtime_rate
+        return round(overtime_value, 2)
+            
+#? deve ter um método do GERENTE p aprovar as ferias:
+
+    def request_vacation(self):
+        if self.__status_employee == "Ferias":
+            return "O funcionário se encontra em recesso."
+  
+        vacation_request = {
+            "employee": self.__name,
+            "employee_id": self.__id_employee,
+            "departament": self.__departament,
+            "request_date": datetime.now(),
+            "status": "Aguardando a avaliação do Gerente"
+        }
+        
+        return {
+            "mensagem": "Olá, a sua solicitação de férias foi registrada com sucesso!",
+            "despacho": "O pedido se encontra na análise do Gerente",
+            "detalhes": vacation_request
+        }
+    
+    
+    def request_raise(self, percentage):
+        if percentage <= 0:
+            return "Este percentual de aumento é inválido."
+        
+        request_raise = {
+            "employee": self.__name,
+            "employee_id": self.__id_employee,
+            "current_salary": self.__salary,
+            "request_percentage": percentage,
+            "request_date": datetime.now(),
+            "status": "Aguardando a avaliação do Gerente"
+        }
+        
+    #regra: acima de 15% precisa de aprovaçãoo do gerente:
+        if percentage > 15:
+            request_raise["Observação"] = "Aumentos acima de 15% necessitam da análise da Gerência"
+        
+        return {
+            "mensagem": "Olá, o seu pedido de aumento salarial foi registrada com sucesso!",
+            "despacho": "O pedido se encontra na análise do Gerente",
+            "detalhes": request_raise
+        }
+    
+        
 class Product:
     def __init__(self, nome, quantidade, preco, dataValidade):
         self.__name = nome
